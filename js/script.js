721dbbec05e0faae8bb780762ec4528bd7939928
txoxu <king-crimson05@outlook.jp>
2024-11-11 19:23:17 +0900

    style.cssとscript.jsを追加

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const text = document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+
+const colorBg = () => {
+  document.body.style.backgroundColor =  color.value;
+
+  if  (color.value === '#ffffff') {
+    text.textContent = 'カラーコード: ${color.value} (white)';
+  } else if (color.value === '#000000') {
+    text.textContent = 'カラーコード: ${color.value} (black)';
+  } else {
+    text.textContent = 'カラーコード: ${color.value}';
+  }
+};
+
+color.addEventListener('input, colorBg');
