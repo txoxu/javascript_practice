f414fada78e8681ca2a9f10a00f8fcfbc0b270ac
txoxu <king-crimson05@outlook.jp>
2024-12-02 01:27:13 +0900

    分割代入とアニメーションを実験

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
-const text = document.querySelector('#colorText');
-const color = document.querySelector('#colorPicker');
+const menu = document.querySelector('#menu');
 
-const colorBg = () => {
-  document.body.style.backgroundColor = color.value;
+const lists = [
+  { image: 'エリカ.webp', name: 'エリカ' },
+  { image: 'ゲーリック・コーヒー.webp', name: 'ゲーリック・コーヒー'},
+  { image: 'スコッチ・コリンズ.webp', name: 'スコッチ・コリンズ'},
+  { image: 'チャーチル.webp', name: 'チャーチル'},
+  { image: 'ラスティ・ネイル.webp', name: 'ラスティ・ネイル'},
+  { image: 'ロブ・ロイ.webp',name: 'ロブ・ロイ'},
+];
 
-  if (color.value === '#ffffff') {
-    text.textContent = `カラーコード:${color.value} (white)`;
-  } else if (color.value === '#000000') {
-    text.textContent = `カラーコード:${color.value} (black)`;
-  } else {
-    text.textContent = `カラーコード:${color.value}`;
-  }
+for (i = 0; i < lists.length; i++) {
+
+  const {image, name} = lists[i];
+  const content = `<div><img src="images/${image}" alt=""><p>${name}</p></div>`;
+
+  menu.insertAdjacentHTML('beforeend', content);
+}
+
+const heading = document.querySelector('#heading');
+
+const keyframes = {
+  color: ['transparent', '#fff'],
+  backgroundPosition: ['100% 0', '0 0'],
 };
 
-color.addEventListener('input', colorBg);
+const options  = {
+  duration: 2000,
+  easing: 'ease',
+};
 
-
-
+heading.animate(keyframes, options);
